1b7212858c26a27a63f9714309a20e0a55091df5
Erik Sabirzhanov (esabirzh) <esabirzh@cisco.com>
2017-12-18 15:59:10 -0500

    first java code

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * Created by esabirzh on 12/18/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
