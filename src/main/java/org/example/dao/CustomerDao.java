59a0315330e0a1a4d5cf8b1ad898a0be12972c63
Erik Sabirzhanov (esabirzh) <esabirzh@cisco.com>
2018-01-03 12:22:54 -0500

    a small spring based web app created

---
+++ src/main/java/org/example/dao/CustomerDao.java
@@ -0,0 +1,70 @@
+package org.example.dao;
+
+import org.example.model.Customer;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CustomerDao {
+    // Dummy database. Initialize with some dummy values.
+    private static List<Customer> customers;
+    {
+        customers = new ArrayList();
+        customers.add(new Customer(101, "John", "Doe", "djohn@gmail.com", "121-232-3435"));
+        customers.add(new Customer(201, "Russ", "Smith", "sruss@gmail.com", "343-545-2345"));
+        customers.add(new Customer(301, "Kate", "Williams", "kwilliams@gmail.com", "876-237-2987"));
+        customers.add(new Customer(System.currentTimeMillis(), "Viral", "Patel", "vpatel@gmail.com", "356-758-8736"));
+    }
+
+    public List list() {
+        return customers;
+    }
+
+
+    public Customer get(Long id) {
+
+        for (Customer c : customers) {
+            if (c.getId().equals(id)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+
+    public Customer create(Customer customer) {
+        customer.setId(System.currentTimeMillis());
+        customers.add(customer);
+        return customer;
+    }
+
+
+    public Long delete(Long id) {
+
+        for (Customer c : customers) {
+            if (c.getId().equals(id)) {
+                customers.remove(c);
+                return id;
+            }
+        }
+
+        return null;
+    }
+
+
+    public Customer update(Long id, Customer customer) {
+
+        for (Customer c : customers) {
+            if (c.getId().equals(id)) {
+                customer.setId(c.getId());
+                customers.remove(c);
+                customers.add(customer);
+                return customer;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/org/example/model/Customer.java
@@ -0,0 +1,57 @@
+package org.example.model;
+
+import java.util.Date;
+
+/**
+ * Created by esabirzh on 1/3/18.
+ */
+public class Customer {
+    private Long id;
+    private String firstName;
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String lastName;
+    private String email;
+    private String mobile;
+    private Date dateOfBirth;
+
+    public Customer(long id, String firstName, String lastName, String email, String mobile) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.mobile = mobile;
+        this.dateOfBirth = new Date();
+    }
+
+
+
+}
