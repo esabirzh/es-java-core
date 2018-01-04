1d60bd12eb111967c01af22808d80b71987359d7
Erik Sabirzhanov (esabirzh) <esabirzh@cisco.com>
2018-01-04 09:03:31 -0500

    added the dispatcher servlet configuration

---
+++ src/main/java/org/example/config/AppInitializer.java
@@ -0,0 +1,21 @@
+package org.example.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/* the dispatcher servlet using standard Java based configuration instead of the older web.xml */
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class[] getRootConfigClasses() {
+        return new Class[] { AppConfig.class };
+    }
+
+    @Override
+    protected Class[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
